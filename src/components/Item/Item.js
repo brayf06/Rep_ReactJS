d1fd772afe2c7065bfe5384b5a83f7b8f07dbc7e
Bryan Formento <brayanformento9@gmail.com>
2023-05-07 13:14:15 -0300

    Se modifica tamaño de items y se agrega map de un array de items

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,14 +1,11 @@
-import image1 from './img/asia.jpg'
-
-function Item (props){
+function Item ({title, imgSRC, description}){
     return(
         <>
-         <h1>{props.textoRecibido}</h1>
-         <div className="card text-center bg-dark ">
-            <img src={image1} alt=""/>
+         <div className="card text-center bg-dark mw-50">
+            <img className="card-img-top" src={imgSRC} alt=""/>
             <div className="card-body text-light">
-                <h4 className="card-title">Titulo</h4>
-                <p className="card-text text-secondary">lorem</p>
+                <h4 className="card-title">{title}</h4>
+                <p className="card-text text-secondary">{description}</p>
                 <a href="#!" className="btn btn-outline-secondary rounded-0">
                 Agregar al carrito    
                 </a> 
--- src/components/Item/Items.js
+++ src/components/Item/Items.js
@@ -1,22 +1,44 @@
 import React from 'react'
 import Item from './Item'
 
+import image1 from './img/asia.jpg'
+import image2 from './img/caribe.jpg'
+import image3 from './img/europa.jpg'
+
+const items = [
+    {
+        id: 1,
+        title: "Descubre Asia",
+        description: "Descubre algunos de los mejores turismos en Asia al mejor precio, solicita cotizacion agregando el item al carrito",
+        image: image1
+    },
+    {
+        id: 2,
+        title: "Viaje al Caribe",
+        description: "Disfruta de las playas paradisiacas del caribe al mejor precio, solicita cotizacion agregando el item al carrito",
+        image: image2
+    },
+    {
+        id: 3,
+        title: "Conoce Europa",
+        description: "Conoce algunas de las mejores ciudades europeas del mundo al mejor precio, solicita cotizacion agregando el item al carrito:",
+        image: image3
+    }
+]
+
 function Items(){
     return (
-        <div className='container d-flex justify-content-center'>
+        <div className='container d-flex justify-content-center align-items-center p-5'>
             <div className='row'>
-                <div className='col-md-4'>
-                    <Item />
-                </div>
-                <div className='col-md-4'>
-                    <Item />
-                </div>
-                <div className='col-md-4'>
-                    <Item />
+                {
+                    items.map(item => (
+                        <div className='col-md-4' key={item.id}>
+                        <Item title={item.title} imgSRC={item.image} description={item.description}/>
+                        </div>
+                    ))
+                }
                 </div>
             </div>
-            
-        </div>
     )
 }
 
